0eb2849cf965a1b7b200f5c9b724cdab6781c6cc
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-16 18:50:07 +0530

    file updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Welcome to the nestjs world';
+    return 'Hello World! Welcome to the NestJS world';
   }
 }
