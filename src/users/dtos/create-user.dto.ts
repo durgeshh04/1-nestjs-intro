8911bbcc086c5bbea1a07230a9f0995613d953a0
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-31 21:41:40 +0530

    dto added for creating user

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto{
+    id: number;
+    name: string;
+    gender: string;
+    email: string;
+    isMarried: boolean;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,38 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  getUsers(@Query() query: any) {
+  getUsers(
+    @Query() query: any,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(query);
     return this.usersService.getUsers(query);
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string) {
-    return this.usersService.getUserById(Number(id));
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserById(id);
   }
 
   @Post()
-  createUser(
-    @Body() user: { name: string; gender: string; isMarried: boolean },
-  ) {
+  createUser(@Body() user: CreateUserDto) {
     return this.usersService.createUser(user);
   }
 }
