e6bd8344fbbd79429d712a7f2bfe5a66a047d80a
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-03-31 22:38:33 +0530

    fix:(create-user.dto.ts) file

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,7 +1,27 @@
-export class CreateUserDto{
-    id: number;
-    name: string;
-    gender: string;
-    email: string;
-    isMarried: boolean;
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3, { message: 'name should be have minimum 3 characters' })
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  gender: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: `email can't be empty` })
+  email: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isMarried: boolean;
 }
